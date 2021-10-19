bcade06b6cf7ad03a37e9cc9bf3d34116171f05f
Josip-github <josipcota1@gmail.com>
2021-10-19 09:09:04 +0200

    This commit creates MessageService class It's located in package service because of separation of concerns principle (it's not good to overload the controller unnecessary); in this class business logis is manifested

---
+++ src/main/java/com/example/basicchatapp/service/MessageService.java
@@ -0,0 +1,39 @@
+package com.example.basicchatapp.service;
+
+import com.example.basicchatapp.model.ChatForm;
+import com.example.basicchatapp.model.ChatMessage;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MessageService {
+    private List<ChatMessage> messages;
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Creating MessageService bean");
+        messages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatForm chatForm){
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setUsername(chatForm.getUsername());
+        if(chatForm.getMessageType().equals("Say")){
+            newMessage.setMessage(chatForm.getMessageText());
+        }
+        if(chatForm.getMessageType().equals("Shout")){
+            newMessage.setMessage(chatForm.getMessageText().toUpperCase());
+        }
+        if(chatForm.getMessageType().equals("Whisper")){
+            newMessage.setMessage(chatForm.getMessageText().toLowerCase());
+        }
+        this.messages.add(newMessage);
+    }
+
+    public List<ChatMessage> getChatMessages(){
+        return messages;
+    }
+}
