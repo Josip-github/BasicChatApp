2849149cd078f3a226f59b421ad647b7b696961a
Josip-github <josipcota1@gmail.com>
2021-10-19 08:39:43 +0200

    Initial commit Maven dependencies: Spring Web, Spring DevTools, Thymeleaf

---
+++ src/main/java/com/example/basicchatapp/BasicChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicchatapp/BasicChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
