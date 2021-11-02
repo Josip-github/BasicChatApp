3909cbe64f48cf25c6c3fe31545bff456f094f16
Josip-github <josipcota1@gmail.com>
2021-11-02 19:11:12 +0100

    This commit creates AuthenticationService class It depends on userMapper and HashService, how and why we'll see in next commit by defining authenticate() method. This class implements AuthenticationProvider interface which means there are some methods that have to be implemented, i.e. in this case authenticate() and supports()

---
+++ src/main/java/com/example/basicchatapp/service/AuthenticationService.java
@@ -0,0 +1,29 @@
+package com.example.basicchatapp.service;
+
+import com.example.basicchatapp.repository.UserMapper;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+
+    private UserMapper userMapper;
+    private HashService hashService;
+
+    public AuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+}
