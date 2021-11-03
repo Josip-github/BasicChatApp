44f697490883c61ba545b5f7dbecd88e03a90cd8
Josip-github <josipcota1@gmail.com>
2021-11-03 07:06:42 +0100

    This commit LoginController class

---
+++ src/main/java/com/example/basicchatapp/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.basicchatapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    public String loginView(){
+        return "login";
+    }
+}
