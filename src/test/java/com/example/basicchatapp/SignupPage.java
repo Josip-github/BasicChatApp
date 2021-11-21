4db4e82158b95901979f4f7dd97e28d8ec55d134
Josip-github <josipcota1@gmail.com>
2021-11-21 15:48:18 +0100

    This commit adds WebElement fields to the Selenium's Page Object SignupPage they should be found by id in the template

--- src/test/java/com/example/basicchatapp/SignupPage.java
+++ src/test/java/com/example/basicchatapp/SignupPage.java
@@ -1,4 +1,27 @@
 package com.example.basicchatapp;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class SignupPage {
+
+    @FindBy(id = "inputFirstName")
+    private WebElement firstnameField;
+
+    @FindBy(id = "inputLastName")
+    private WebElement lastnameField;
+
+    @FindBy(id = "inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+
+
+
+
 }
