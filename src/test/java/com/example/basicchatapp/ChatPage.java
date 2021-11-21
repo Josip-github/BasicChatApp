7edf8251687983b488bb2cf9f092b1478c67fbb9
Josip-github <josipcota1@gmail.com>
2021-11-21 16:44:52 +0100

    This commit adds web elements to the Selenium's class ChatPage Also, since some elements weren't specifically determined in the template, I added it to the template.

--- src/test/java/com/example/basicchatapp/ChatPage.java
+++ src/test/java/com/example/basicchatapp/ChatPage.java
@@ -1,4 +1,22 @@
 package com.example.basicchatapp;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ChatPage {
+
+    @FindBy(id = "messageText")
+    private WebElement messageTextField;
+
+    @FindBy(id = "submitMessage")
+    private WebElement submitButton;
+
+    @FindBy(className = "chatMessageUsername")
+    private WebElement firstMessageUsername;
+
+    @FindBy(className = "chatMessageText")
+    private WebElement firstMessageText;
+
+
+
 }
