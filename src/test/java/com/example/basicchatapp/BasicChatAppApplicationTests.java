241b3ba0029da1860fea7df8ddb7687893007c55
Josip-github <josipcota1@gmail.com>
2021-11-21 17:11:50 +0100

    This commit creates fields and methods in class for application tests The methods are annotated with @BeforeAll(setting up the driver), @AfterAll(quitting the driver) and @BeforeEach(defining the baseURL for all the templates)

--- src/test/java/com/example/basicchatapp/BasicChatAppApplicationTests.java
+++ src/test/java/com/example/basicchatapp/BasicChatAppApplicationTests.java
@@ -1,6 +1,12 @@
 package com.example.basicchatapp;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -10,7 +16,25 @@ class BasicChatAppApplicationTests {
     @LocalServerPort
     private Integer port;
 
+    private static WebDriver driver;
 
+    private String baseURL;
+
+    @BeforeAll
+    public static void beforeAll(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        driver.quit();
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        baseURL = "http://localhost:" + port;
+    }
 
     @Test
     void contextLoads() {
