3125fbcf0092b28c566f853ae0eaf9a252011927
Josip-github <josipcota1@gmail.com>
2021-11-21 16:26:58 +0100

    This commit creates constructor in LoginPage It relies on PageFactory's static method initElements()

--- src/test/java/com/example/basicchatapp/LoginPage.java
+++ src/test/java/com/example/basicchatapp/LoginPage.java
@@ -1,7 +1,9 @@
 package com.example.basicchatapp;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
@@ -14,6 +16,7 @@ public class LoginPage {
     @FindBy(id = "submit-button")
     private WebElement submitButton;
 
-
-
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
