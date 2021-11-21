371a07641eaa896f8d7d3722ce2a2decce28203c
Josip-github <josipcota1@gmail.com>
2021-11-21 17:22:35 +0100

    This commit creates testSignupLoginAndSubmitingMessage() method In this methos everything that has been developed in Selenium Page Objecct classes (SignupPage, LoginPage, ChatPage) has been tested.

--- src/test/java/com/example/basicchatapp/BasicChatAppApplicationTests.java
+++ src/test/java/com/example/basicchatapp/BasicChatAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.basicchatapp;
 
+import com.example.basicchatapp.model.ChatForm;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BasicChatAppApplicationTests {
@@ -34,6 +37,29 @@ class BasicChatAppApplicationTests {
     @BeforeEach
     public void beforeEach(){
         baseURL = "http://localhost:" + port;
+    }
+
+    @Test
+    public void testSignupLoginAndSubmitingMessage(){
+        String username = "darthVader";
+        String password = "forceAwakens";
+        String messageText = "Let the force be with you";
+
+        driver.get(baseURL + "/signup");
+        SignupPage signupPage = new SignupPage(driver);
+        signupPage.signup("Anakin", "Skywalker", username, password);
+
+        driver.get(baseURL + "/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(username, password);
+
+        ChatPage chatPage = new ChatPage(driver);
+        chatPage.submitNewMessage(messageText);
+
+        ChatForm sentMessage = chatPage.getFirstMessage();
+
+        assertEquals(username, sentMessage.getUsername());
+        assertEquals(messageText, sentMessage.getMessageText());
     }
 
     @Test
