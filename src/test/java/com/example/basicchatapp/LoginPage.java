80b0e50cf5a36f272a2998d13dcef508deebde73
Josip-github <josipcota1@gmail.com>
2021-11-21 16:25:41 +0100

    This commit creates 3 new fields in the Selenium's Page object LoginPage

--- src/test/java/com/example/basicchatapp/LoginPage.java
+++ src/test/java/com/example/basicchatapp/LoginPage.java
@@ -1,4 +1,19 @@
 package com.example.basicchatapp;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy(name = "username")
+    private WebElement usernameField;
+
+    @FindBy(name = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+
+
 }
